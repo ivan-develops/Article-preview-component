c45cf423add866a85557c6fb2fece253e7f79bc3
Iván Vargas O <ivanvargas.va@gmail.com>
2025-08-18 23:59:38 -0500

    challenge complete

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const modal = document.getElementById('modal');
 const btnShare = document.getElementById('btn-share');
 const modal__btnShare = modal.querySelector(".btn-share");
 
-btnShare.addEventListener('click' , function(){
-    modal.style.visibility = "visible";
-}); 
-
 modal__btnShare.addEventListener( 'click' , ()=>{
-    modal.style.visibility = "hidden";
-})
+    modal.classList.toggle('active');
+});
+
+btnShare.addEventListener( 'click' , ()=>{
+    modal.classList.toggle('active');
+} );
